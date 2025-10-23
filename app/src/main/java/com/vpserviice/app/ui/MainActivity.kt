de1322b25f7fed213109dac785612a54e391a76c
EsiFix <esi.samadi71@gmail.com>
2025-10-23 10:15:02 +0330

    Create MainActivity.kt

---
+++ app/src/main/java/com/vpserviice/app/ui/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.vpserviice.app.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.vpserviice.app.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.title.text = "VPSERVIICE"
+        binding.subtitle.text = "Android VPN Client"
+
+        binding.btnLogin.setOnClickListener {
+            // TODO: صفحه Login → Cloudius/RADIUS
+        }
+
+        binding.btnServers.setOnClickListener {
+            // TODO: لیست سرورها + Connect/Disconnect
+        }
+    }
+}
