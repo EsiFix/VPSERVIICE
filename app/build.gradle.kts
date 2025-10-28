a0e543472f547650b8dbee70d3f86cc4554bbd94
EsiFix <esi.samadi71@gmail.com>
2025-10-28 17:15:45 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.github.EsiFix:openvpnlib:v0.7.62-lib1")
+    implementation("com.github.EsiFix:openvpnlib:v0.7.62-lib3")
 
     // موتور OpenVPN و شبکه را بعداً اضافه می‌کنیم
 }
