9a53dc8b6f1c6c83a330e4a319e098fe2303ee21
EsiFix <esi.samadi71@gmail.com>
2025-10-31 07:09:42 +0330

    feat: create MainActivity for VPN application

---
+++ app/src/main/java/com/vpservice/android/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.vpservice.android.MainActivity
+import android.text.TextComposite
+implementation class MainActivity: MainActivation {
+    private lang on creation context.ManagingActivity
+    value value {
+        super.on Creation.context.ManagingActivity
+    }
+}
