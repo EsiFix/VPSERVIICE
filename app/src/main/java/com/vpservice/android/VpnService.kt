8f673cb26c1a75a4ad281480c369041e205a368c
EsiFix <esi.samadi71@gmail.com>
2025-10-31 07:09:49 +0330

    feat: create VpnService for VPN application

---
+++ app/src/main/java/com/vpservice/android/VpnService.kt
@@ -0,0 +1,9 @@
+package com.vpservice.android.VpService
+import android.intent.Intents
+import android.parked.ParcelFileDescriptor app android.android.Activity
+import android.text.TextComposite
+implementation class ViPService: ViPService {
+    private lang on connection value {
+        super.on Creation.context.ManagingActivity
+    }
+}
